fc75b6a09b44ad6848d08a5dca7334e5dc2b3b60
jalfje <jamiestm9@hotmail.com>
2017-03-01 15:24:17 -0800

    Put functionality of Point class into Node class. Added failure conditions for A* algorithm.

--- app/main.py
+++ app/main.py
@@ -8,31 +8,51 @@ turn = 0
 snakes = []
 food = []
 snakeLocs = []
+failureValue = -1
 
 """
 ------------------
 --- MAIN LOGIC ---
 ------------------
 """
-class Point:
-    def __init__(self, x=0, y=0):
+class Node:
+    def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.parent = None
+        self.G = 0
+        self.H = 0
+        self.setValue()
+
+    def moveCost(self, other):
+        if self.value == '%':
+            return 10
+        else:
+            return self.value
     
-    # Returns the distance between itself and another point
+    def setValue(self):
+        if (self.x < 0 or self.y < 0 or self.x >= board_width or self.y >= board_height):
+            self.value = '%'
+        elif any(self == s for s in snakeLocs):
+            self.value = '%'
+        elif any(self == f for f in food):
+            self.value = -2
+        else:
+            self.value = 1
+    
+    # Returns the distance between itself and another Node - "Manhattan" distance
     # Uses x distance + y distance, not actual (diagonal) distance, because snakes only move N/S/E/W   
     def distance(self, other):
         return abs(self.x - other.x) + abs(self.y - other.y)
+        
     # Finding direction to go to get to self from other - up,down,left,right correspond to 0,1,2,3
     # Works like final - initial with vectors - e.g. (1,0) - (0,0) = 3 -> move right
     def __sub__(self, other):
-        # if other is to the left of self, the displacement is to the right
+        # if other is to the left of self, the displacement is to the right. others are similar.
         if self.x - other.x == 1:
             return 3
-        # if other is to the right of self, the displacement is to the left 
         if self.x - other.x == -1:
             return 2
-        # down is positive y, up is negative y.
         if self.y - other.y == 1:
             return 1
         if self.y - other.y == -1:
@@ -40,58 +60,21 @@ class Point:
         else:
             return NotImplemented
     def __str__(self):
-        return "({},{})".format(self.x,self.y)
+        return "({},{}):{}".format(self.x,self.y,self.value)
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
+    # Hash function required due to overloaded == operator, and Node's use within sets.
+    # This hash function guarantees no collisions with non-equal Nodes. x * height > y for all x,y.
     def __hash__(self):
         return self.x * board_height + self.y
-        
-class Node:
-    def __init__(self, point, value):
-        self.value = value
-        self.point = point
-        self.parent = None
-        self.G = 0
-        self.H = 0
-
-    def moveCost(self, other):
-        if self.value == '%':
-            return 1
-        elif self.value == '!':
-            return -1
-        else:
-            return 0
-
-    def heuristic(self, other):
-        return self.point.distance(other.point)
-    
-    def __sub__(self, other):
-        return self.point - other.point
-    def __str__(self):
-        return "{}:{}".format(self.point,self.value)
-    def __eq__(self, other):
-        return self.point == other.point
-    def __hash__(self):
-        return self.point.x * board_height + self.point.y
-
-def valueOf(point):
-    value = '.'
-    if (point.x < 0 or point.y < 0 or point.x >= board_width or point.y >= board_height):
-        value = '%'
-    elif any(point == s for s in snakeLocs):
-        value = '%'
-    elif any(point == f for f in food):
-        value = '!'
-    return value
-
 
 def children(node):
-    x,y = node.point.x,node.point.y
+    x,y = node.x,node.y
     children = []
-    points = [Point(x-1, y), Point(x+1, y), Point(x, y-1), Point(x, y+1)]
-    for p in points:
-        if valueOf(p) != '%':
-            children.append(Node(p, valueOf(p)))
+    nodes = [Node(x-1,y), Node(x+1,y), Node(x,y-1), Node(x,y+1)]
+    for n in nodes:
+        if n.value != '%':
+            children.append(n)
     return children
 
 def AStar(head, goalNode):
@@ -124,28 +107,37 @@ def AStar(head, goalNode):
                     node.parent = current
             else:
                 node.G = current.G + current.moveCost(node)
-                node.H = node.heuristic(goalNode)
+                node.H = node.distance(goalNode)
                 node.parent = current
                 openSet.add(node)
-    
+    #TODO: Handle no path found (i.e. the goal is blocked off. Should try to find another node.)
     print("Path not found. Uh oh.")
+    return failureValue
   
 def chooseGoalNode(head):
     #TODO: Fix this up to actually decide where it's best to go to.
-    rand = Node(Point(random.randint(0,board_width-1), random.randint(0,board_height-1)), '!')
-    rand = Node(food[0], '!')
-    return rand
+    #goal = bestFood() else goal = center()
+    goal = Node(random.randint(0,board_width-1), random.randint(0,board_height-1))
+    goal = food[0]
+    return goal
     
 def moveToGoalNode(head, goalNode):
-    bestMoves = AStar(head, goalNode)
-    return bestMoves[1]
+    astar = AStar(head, goalNode)
+    # AStar may return a failureValue to indicate it can't find a path. If so, return failureValue
+    # so that we can try again, with a new goal node.
+    if (astar != failureValue):
+        return astar[1]
+    else:
+        return failureValue
 
 # Returns an int between 0 and 3, inclusive
 def choose(head):
-    goalNode = chooseGoalNode(head)
-    print("Goal node: ",str(goalNode))
-    nextMove = moveToGoalNode(head, goalNode)
-    print("Next move: ",str(nextMove))
+    nextMove = failureValue
+    while nextMove == failureValue:
+        goalNode = chooseGoalNode(head)
+        print("Goal node: ",str(goalNode))
+        nextMove = moveToGoalNode(head, goalNode)
+        print("Next move: ",str(nextMove))
     return nextMove - head
     
 """
@@ -194,11 +186,11 @@ def move():
     snakes = data['snakes']
     
     for f in foodlist:
-        food.append(Point(f[0],f[1]))
+        food.append(Node(f[0],f[1]))
     
     for s in snakes:
         for c in s['coords']:
-            snakeLocs.append(Point(c[0],c[1]))
+            snakeLocs.append(Node(c[0],c[1]))
         
     #print(data)
     print(our_snake)
@@ -206,8 +198,7 @@ def move():
     print(snakes['id'==our_snake]['taunt'])
     print "snake locations: [%s]" % ", ".join(map(str, snakeLocs))
     
-    headp = Point(snakes['id'==our_snake]['coords'][0][0], snakes['id'==our_snake]['coords'][0][1])
-    head = Node(headp, '%')
+    head = Node(snakes['id'==our_snake]['coords'][0][0], snakes['id'==our_snake]['coords'][0][1])
     direction = choose(head)
     directions = ['up', 'down', 'left', 'right']
 
