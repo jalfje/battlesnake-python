44dbee771ec75c6142861aae899cb30b71f06b28
jalfje <jamiestm9@hotmail.com>
2017-03-01 13:37:02 -0800

    First working version of A* algorithm working. Determining goal node is next step.

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ board_height = 0
 turn = 0
 snakes = []
 food = []
+snakeLocs = []
 
 """
 ------------------
@@ -20,8 +21,30 @@ class Point:
     
     # Returns the distance between itself and another point
     # Uses x distance + y distance, not actual (diagonal) distance, because snakes only move N/S/E/W   
-    def distance(self, point):
-        return abs(self.x - point.x) + abs(self.y - point.y)
+    def distance(self, other):
+        return abs(self.x - other.x) + abs(self.y - other.y)
+    # Finding direction to go to get to self from other - up,down,left,right correspond to 0,1,2,3
+    # Works like final - initial with vectors - e.g. (1,0) - (0,0) = 3 -> move right
+    def __sub__(self, other):
+        # if other is to the left of self, the displacement is to the right
+        if self.x - other.x == 1:
+            return 3
+        # if other is to the right of self, the displacement is to the left 
+        if self.x - other.x == -1:
+            return 2
+        # down is positive y, up is negative y.
+        if self.y - other.y == 1:
+            return 1
+        if self.y - other.y == -1:
+            return 0
+        else:
+            return NotImplemented
+    def __str__(self):
+        return "({},{})".format(self.x,self.y)
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    def __hash__(self):
+        return self.x * board_height + self.y
         
 class Node:
     def __init__(self, point, value):
@@ -32,29 +55,34 @@ class Node:
         self.H = 0
 
     def moveCost(self, other):
-        if value == '%':
+        if self.value == '%':
             return 1
-        elif value == '!':
+        elif self.value == '!':
             return -1
         else:
             return 0
-    
-    def heuristic(self, other):
-        return distance(self.point, other.point)
 
-def getNextClosestFood(foodList):
-    closestFood = foodList[0]
-    return closestFood
+    def heuristic(self, other):
+        return self.point.distance(other.point)
+    
+    def __sub__(self, other):
+        return self.point - other.point
+    def __str__(self):
+        return "{}:{}".format(self.point,self.value)
+    def __eq__(self, other):
+        return self.point == other.point
+    def __hash__(self):
+        return self.point.x * board_height + self.point.y
 
 def valueOf(point):
+    value = '.'
     if (point.x < 0 or point.y < 0 or point.x >= board_width or point.y >= board_height):
         value = '%'
-    elif any(point in s for s in snakes):
+    elif any(point == s for s in snakeLocs):
         value = '%'
-    elif (point in f):
+    elif any(point == f for f in food):
         value = '!'
-    else:
-        value = '.'
+    return value
         
 
 def children(node):
@@ -63,7 +91,7 @@ def children(node):
     points = [Point(x-1, y), Point(x+1, y), Point(x, y-1), Point(x, y+1)]
     for p in points:
         if valueOf(p) != '%':
-            children.add(Node(p, valueOf(p)))
+            children.append(Node(p, valueOf(p)))
     return children
 
 def AStar(head, goalNode):
@@ -96,26 +124,29 @@ def AStar(head, goalNode):
                     node.parent = current
             else:
                 node.G = current.G + current.moveCost(node)
-                node.H = distance(node.point, goalNode)
+                node.H = node.heuristic(goalNode)
                 node.parent = current
-                openset.add(node)
+                openSet.add(node)
     
     print("Path not found. Uh oh.")
-                
-        
-        
+  
 def chooseGoalNode(head):
-    return head
+    #TODO: Fix this up to actually decide where it's best to go to.
+    rand = Node(Point(random.randint(0,board_width-1), random.randint(0,board_height-1)), '!')
+    rand = Node(food[0], '!')
+    return rand
     
 def moveToGoalNode(head, goalNode):
     bestMoves = AStar(head, goalNode)
-    return bestMoves[0]
+    return bestMoves[1]
 
 # Returns an int between 0 and 3, inclusive
 def choose(head):
     goalNode = chooseGoalNode(head)
+    print("Goal node: ",str(goalNode))
     nextMove = moveToGoalNode(head, goalNode)
-    return random.randint(0,3)
+    print("Next move: ",str(nextMove))
+    return nextMove - head
     
 """
 ------------------------
@@ -155,17 +186,25 @@ def move():
     data = bottle.request.json
     game_id = data['game_id']
     our_snake = data['you']     # UUID to be searched for in snakes[]
-    global board_width,board_height,turn,food,snakes
+    global board_width,board_height,turn,food,snakes,snakeLocs
     board_width = data['width']
     board_height = data['height']
     turn = data['turn'] # current game turn
-    food = data['food']
+    foodlist = data['food']
     snakes = data['snakes']
     
+    for f in foodlist:
+        food.append(Point(f[0],f[1]))
+    
+    for s in snakes:
+        for c in s['coords']:
+            snakeLocs.append(Point(c[0],c[1]))
+        
     #print(data)
     print(our_snake)
-    print(food)
+    print(str(food))
     print(snakes['id'==our_snake]['taunt'])
+    print "snake locations: [%s]" % ", ".join(map(str, snakeLocs))
     
     headp = Point(snakes['id'==our_snake]['coords'][0][0], snakes['id'==our_snake]['coords'][0][1])
     head = Node(headp, '%')
