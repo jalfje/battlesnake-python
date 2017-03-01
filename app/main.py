9d70be701550fcbaedbcfea8d07f0d6a2f1d6880
jalfje <jamiestm9@hotmail.com>
2017-03-01 01:02:24 -0800

    Deleted empty file. Fixed up main.py to the point where we can start coding properly.

--- app/main.py
+++ app/main.py
@@ -2,6 +2,38 @@ import bottle
 import os
 import random
 
+"""
+------------------
+--- MAIN LOGIC ---
+------------------
+"""
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+    
+    # Returns the distance between itself and another point
+    # Uses x distance + y distance, not actual (diagonal) distance, because snakes only move N/S/E/W   
+    def distance(self, point):
+        return abs(self.x - point.x) + abs(self.y - point.y)
+
+def getNextClosestFood(foodList):
+    closestFood = foodList[0]
+    return closestFood
+
+# Returns an int between 0 and 3, inclusive
+def choose():
+    return random.randint(0,3)
+    
+def moveToGoalPoint(goalPoint):
+    return
+
+
+"""
+------------------------
+--- DEALING WITH I/O ---
+------------------------
+"""
 @bottle.route('/static/<path:path>')
 def static(path):
     return bottle.static_file(path, root='static/')
@@ -40,9 +72,10 @@ def move():
     food = data['food']
     snakes = data['snakes']
     
-    print(food)
-    print(snakes)
-   
+    #print(data)
+    print(our_snake)
+    #print(food)
+    print(snakes['id'==our_snake]['taunt'])
     
     direction = choose()
     directions = ['up', 'down', 'left', 'right']
@@ -52,38 +85,8 @@ def move():
         'taunt': 'battlesnake-python?'
     }
 
+# Jamie: No idea what this comment means or how the function works, but it's best not to break it.
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 if __name__ == '__main__':
     bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
-
-# Main logic starts here.
-class Point:
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-    
-    # Returns the distance between itself and another point
-    # Uses x distance + y distance, not actual (diagonal) distance, because snakes only move N/S/E/W   
-    def distance(self, point):
-        return abs(self.x - point.x) + abs(self.y - point.y)
-
-def getNextClosestFood():
-
-# Returns an int between 0 and 3, inclusive
-def directions():
-    return random(0,3)
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
